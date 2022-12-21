95fd36ecba0ecca7daae1e003297e3ef9fcf4cad
Daniel Simiyu <simiyuwdan@gmail.com>
2022-12-21 13:29:54 +0300

    initial commit

---
+++ src/main/java/com/crud/spring_boot_aws_github_cicd/SpringBootAwsGithubCicdApplication.java
@@ -0,0 +1,13 @@
+package com.crud.spring_boot_aws_github_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsGithubCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsGithubCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/spring_boot_aws_github_cicd/SpringBootAwsGithubCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.spring_boot_aws_github_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsGithubCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
